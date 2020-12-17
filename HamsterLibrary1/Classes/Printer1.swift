4c7439813b84eaacd039ac0ad8ab0a9f76b88629
Dzmitry Kopats <dzmitry.kopats@ext.privalia.com>
2020-12-18 01:04:49 +0300

    1.0.1

---
+++ HamsterLibrary1/Classes/Printer1.swift
@@ -0,0 +1,19 @@
+//
+//  Printer1.swift
+//  HamsterLibrary2
+//
+//  Created by Dzmitry Kopats on 12/17/2020.
+//  Copyright (c) 2020 Dzmitry Kopats. All rights reserved.
+//
+
+import Foundation
+
+public class Printer1 {
+    public static func printFirst() {
+        print("first")
+    }
+    
+    public static func printSecond() {
+        print("here we need to use pod2 functional to print 'second'")
+    }
+}
